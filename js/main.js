d788c071a739ba6326e185f61cab6c7550cc7485
javier <javierlopera988@gmail.com>
2020-04-14 23:20:56 +0200

    Usando selectores close #3

--- js/main.js
+++ js/main.js
@@ -12,7 +12,9 @@ $(function () {
 
     let copia;
 
-    let coloresElegidos = document.getElementsByClassName("colorElegido");
+    let coloresElegidos = $(".colorElegido");
+
+    // let coloresElegidos = document.getElementsByClassName("colorElegido");
 
     let colores = ["rgb(255, 0, 0)", "rgb(0, 0, 255)", "rgb(128, 128, 128)",
         "rgb(255, 192, 203)", "rgb(238, 130, 238)", "rgb(0, 0, 0)", "rgb(255, 255, 255)", "rgb(255, 255, 0)"];
